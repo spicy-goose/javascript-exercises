a19f1d1a086e611417e21561e155410f62487899
Franco <GABRIEL.A.FRANCO@INVISTA.COM>
2022-06-14 13:29:14 -0500

    Add initial attempt at removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToModify, ...itemsToDelete){ 
 
+    //declare solution array
+    let solutionArray = [];
+
+    //iterate over arrayToModify
+    for (item of arrayToModify){
+        //verify item is not in itemsToDelete
+        if (!itemsToDelete.includes(item)){
+            //push item to solution array
+            solutionArray.push(item);
+        }
+    }
+    //return solution array
+    return solutionArray
 };
 
 // Do not edit below this line
