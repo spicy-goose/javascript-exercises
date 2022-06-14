d9b766d05670e7217e56c7bee3a3e35acd0355d7
Franco <GABRIEL.A.FRANCO@INVISTA.COM>
2022-06-14 13:46:48 -0500

    Add initial attempt at sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    //validate numbers, return ERROR if not numbers
+    if (!Number.isInteger(number1) || !Number.isInteger(number2) || number1 >= 0 || number2 >= 0){
+        return 'ERROR'
+    }
+    //declare solution
+    let solution = 0;
+        
+    //Declare variable for lower bound and higher bound. Assign lower number to low bound.
+    if (number1<number2){
+        let lowerBound = number1;
+        let higherBound = number2;
+    }else{
+        let lowerBound = number2;
+        let higherBound = number1;   
+    }
 
+    //iterate from lower bound to higher bound
+    for (let i = lowerBound; i <= higherBound; ++i){
+        //add every item to solution
+        solution += i;
+    }
+    //return solution
+    return solution
 };
 
 // Do not edit below this line
