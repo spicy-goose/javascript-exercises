7575c119822995f51caa2a78e5bc640298507453
Franco <GABRIEL.A.FRANCO@INVISTA.COM>
2022-06-14 14:01:00 -0500

    change loop bounds declaration outside if statement to prevent reference error

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,21 @@
 const sumAll = function(number1, number2) {
     //validate numbers, return ERROR if not numbers
-    if (!Number.isInteger(number1) || !Number.isInteger(number2) || number1 >= 0 || number2 >= 0){
+    if (!Number.isInteger(number1) || !Number.isInteger(number2) || number1 < 0 || number2 < 0){
         return 'ERROR'
     }
     //declare solution
     let solution = 0;
         
     //Declare variable for lower bound and higher bound. Assign lower number to low bound.
+    let lowerBound;;
+    let higherBound;
+
     if (number1<number2){
-        let lowerBound = number1;
-        let higherBound = number2;
+         lowerBound = number1;
+         higherBound = number2;
     }else{
-        let lowerBound = number2;
-        let higherBound = number1;   
+         lowerBound = number2;
+         higherBound = number1;   
     }
 
     //iterate from lower bound to higher bound
