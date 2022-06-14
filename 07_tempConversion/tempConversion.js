9dee971862739c7d98f8c710ef63484a212d4cf0
Franco <GABRIEL.A.FRANCO@INVISTA.COM>
2022-06-14 15:56:38 -0500

    Add first attempt at ftoc and ctof functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,29 @@
-const ftoc = function() {
+const ftoc = function(numberToConvert) {
+  //validate tempToConvert is a number
+  if (typeof(numberToConvert) !== 'number'){
+      return 'Not a Number!'
+  }
 
+  //declare solution variable assign value of calculation
+  let solution = (numberToConvert - 32)*(5/9);
+  //round variable to one decimal place
+  solution = Number(solution.toFixed(1));
+  //return value of conversion
+  return solution
 };
 
-const ctof = function() {
+const ctof = function(numberToConvert) {
+  //validate tempToConvert is a number
+  if (typeof(numberToConvert) !== 'number'){
+    return 'Not a Number!'
+}
 
+//declare solution variable assign value of calculation
+let solution = (numberToConvert *(5/9)) + 32;
+//round variable to one decimal place
+solution = Number(solution.toFixed(1));
+//return value of conversion
+return solution
 };
 
 // Do not edit below this line
