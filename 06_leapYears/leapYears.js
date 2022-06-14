f13f318675c36509f6b91ee174bb50094f2cddf7
Franco <GABRIEL.A.FRANCO@INVISTA.COM>
2022-06-14 15:30:36 -0500

    Add first attempt at leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,29 @@
-const leapYears = function() {
-
+const leapYears = function(yearToCheck) {
+    //validate yearToCheck is a positive integer
+    if (!Number.isInteger(yearToCheck) || yearToCheck < 0){
+        //if not, return not a year
+        return 'ERROR'
+    }
+    //check if year is divisible by 4
+    if (yearToCheck%4 == 0){
+        //check if year is divisible by 100
+        if (yearToCheck%100 == 0){
+            //check if year is divisible by 400
+            if (yearToCheck%400 == 0){
+                //return true
+                return true
+            }else{
+            //return false
+            return false
+            }
+        }else{
+        //return true
+        return true
+        }
+    }else{
+        //return false
+        return false
+    }
 };
 
 // Do not edit below this line
