25a468b72deda4247401d9507b1bd249e6282f4e
Franco <GABRIEL.A.FRANCO@INVISTA.COM>
2022-06-14 08:57:34 -0500

    Add initial responses to Exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,27 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numberOfRepeats) {
+
+    //validate string is of string type
+    if (typeOf(stringToRepeat) !== 'string'){
+
+        //if not, return error string
+        return 'ERROR'
+    }
+    //validate numOfRepeats is of type integer
+    if (!numberOfRepeats.isInteger()){
+        //if not, return error string
+        return 'Error'
+    }
+    //declare result string as empty string
+    let resultString = '';
+
+    //iterate from 0 to numberOfRepeats
+    for (let i = 0; i < stringToRepeat; ++i){
+
+        //concatenate new iteration of stringToRepeat to result string variable
+        resultString += stringToRepeat;
+    }
+    //return result string
+    return resultString;
 
 };
 
