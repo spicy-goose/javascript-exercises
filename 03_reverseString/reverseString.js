7d9f5fd690a1a971d489aa61f7136f188a829240
Franco <GABRIEL.A.FRANCO@INVISTA.COM>
2022-06-14 13:06:22 -0500

    Add initial attempt at reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
 
+    //validate input
+    if (typeof(stringToReverse) !== 'string'){
+        // if stringToReverse is not of type string, return not a string
+        return 'Not a String'
+    }
+    //declare solution empty string
+    let solutionString = '';
+
+    //iterate over the length of the string from length of string to 0
+    for (let i = stringToReverse.length-1; i > 0; --i){
+        //concatenate the current character to the solution string
+        solutionString += stringToReverse[i];
+    }
+    //return solution string
+    return solutionString
 };
 
 // Do not edit below this line
